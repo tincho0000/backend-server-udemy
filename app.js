51ec0b022ad72f496e41aeeea4c41283cf905c5a
Martin Bertomeu <tincho0000@gmail.com>
2018-04-27 14:49:48 -0300

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+// Inicializar variables
+var app = express();
+
+// body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion a la BD
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -10,16 +23,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 
-// Inicializar variables
-var app = express();
-
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion relizada correctamente'
-    });
-});
+
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(3000, () => {
---
+++ routes/login.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+var app = express();
+
+var Usuario = require('../models/usuario');
+
+//================================================
+// Obtener todos los usuarios
+//================================================
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioBD) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuarios',
+                errors: err
+            });
+        }
+
+        if (!usuarioBD) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales incorrectas - email',
+                errors: err
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, usuarioBD.password)) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales incorrectas - password',
+                errors: err
+            });
+        }
+
+        // Crear un token!!!
+        usuarioBD.password = ':)';
+        var token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: 14400 }); //4 horas
+
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioBD,
+            token: token,
+            id: usuarioBD.id
+        });
+
+    });
+
+
+});
+
+
+module.exports = app;
